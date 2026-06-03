e72e94980bab7203c4c672bc88270207d8463c16
Aryan Nehete <142139753+aryannehete14@users.noreply.github.com>
2026-06-03 09:18:48 +0530

    Add files via upload

---
+++ React/DEMO-APP/src/components/Greet.jsx
@@ -0,0 +1,11 @@
+function Greet(person){
+    return(
+        <>
+        <h3>Good Afternoon {person.name}</h3>
+        {/* <h3>Good Afternoon Students</h3>
+        <h3>Good Afternoon All</h3> */}
+        </>
+    )
+}
+
+export default Greet
---
+++ React/DEMO-APP/src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+        <h1>This is my Website Heading</h1>
+    )
+}
+
+export default Header
