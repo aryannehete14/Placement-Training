73aaf993c49d8e1922ddf5b5f84edbf29843c8ef
Aryan Nehete <142139753+aryannehete14@users.noreply.github.com>
2026-06-03 11:31:34 +0530

    Update PrimeNumber.cpp

--- C++/PrimeNumber.cpp
+++ C++/PrimeNumber.cpp
@@ -7,10 +7,10 @@ int main(){
     cin>>n;
     if(n<=1){
         cout<<n<<" "<<"is Not Prime Number"<<endl;
-    }else if(n%2==0|| n%3==0){
-        cout<<n<<" "<<"is Not Prime Number"<<endl;
     }else if(n==2||n==3){
         cout<<n<<"is Prime Number"<<endl;
+    }else if(n%2==0|| n%3==0){
+        cout<<n<<" "<<"is Not Prime Number"<<endl;
     }else{
         cout<<n<<"is Prime Number";
     }
