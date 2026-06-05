ae96b09f52373338bb09953ad4528d2e8a29fa07
Aryan Nehete <142139753+aryannehete14@users.noreply.github.com>
2026-06-05 15:08:58 +0530

    Create SortList.py

---
+++ Python/Functions/SortList.py
@@ -0,0 +1,13 @@
+def sort_list(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+numbers = [5, 2, 8, 1, 9]
+
+print(sort_list(numbers))
