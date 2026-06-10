b3d38c38077303e4fedc9c7cb29a06a978817621
Aryan Nehete <142139753+aryannehete14@users.noreply.github.com>
2026-06-10 11:43:31 +0530

    Add BasicCodes.java with main method and logic

---
+++ Java/BasicCodes.java
@@ -0,0 +1,23 @@
+package com.training.demo;
+
+public class BasicCodes {
+
+	public static void main(String[] args) {
+		System.out.println("Print Statement");
+		int a = 201;
+		int b = 301;
+		int c = a + b;
+		System.out.println("Sum of numbers is " + c);
+		if (a > 0) {
+			System.out.println("Positive Number");
+		} else if (a < 0) {
+			System.out.println("Negative Number");
+		} else {
+			System.out.println("Number is Zero");
+		}
+		
+	
+	}
+
+}
+
