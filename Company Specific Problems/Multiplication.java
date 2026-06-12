a21351d39e43afee5e9bcf55608c05eca2934a84
Aryan Nehete <142139753+aryannehete14@users.noreply.github.com>
2026-06-12 09:24:20 +0530

    Create Multiplication.java

---
+++ Company Specific Problems/Multiplication.java
@@ -0,0 +1,10 @@
+package com.CompanySpecificProblems;
+
+// Write a program to generate and display the multiplication table of 2.
+public class Multiplication {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("2 x " + i + " = " + (2 * i));
+		}
+	}
+}
